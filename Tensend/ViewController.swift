ef74ead7e1a708aab9be577a9dad31076271125c
Tair <adibek.t@maint.kz>
2020-01-26 19:52:03 +0600

    first commit

---
+++ Tensend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tensend
+//
+//  Created by Sultan on 1/24/20.
+//  Copyright © 2020 Sultan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
