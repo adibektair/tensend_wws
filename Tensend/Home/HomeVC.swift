9ef68ca4fecdd26fa39c3ea0e45f4558eb0691ee
Sultan <dakanov.s@gmail.com>
2020-01-26 21:02:02 +0600

    update

---
+++ Tensend/Home/HomeVC.swift
@@ -0,0 +1,26 @@
+//
+//  HomeVC.swift
+//  Tensend
+//
+//  Created by Sultan on 1/26/20.
+//  Copyright © 2020 Sultan. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: ScrollStackController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let title = UILabel()
+        title.setProperties( textColor: #colorLiteral(red: 0.2039215686, green: 0.262745098, blue: 0.337254902, alpha: 1), textAlignment: .center)
+        title.text = "Cәлеместіз бе, Қайрат!"
+        stackView.addArrangedSubview(title)
+        let info = UILabel()
+        let infoText = "Tensend-ке саяхаттап, өзіңіз үшін әлемнің шексіз білімін ашыңыз"
+        title.setProperties(text: infoText, textColor: #colorLiteral(red: 0.2039215686, green: 0.262745098, blue: 0.337254902, alpha: 1), textAlignment: .center, numberLines: 2)
+        stackView.addArrangedSubview(info)
+    }
+    
+    
+}
